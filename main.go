86c5017d05dd9a3f62194e20771fbbeafc18eaf9
CH <cedwards2809@gmail.com>
2026-05-09 20:01:48 -0400

    a basic http stream reader

--- main.go
+++ main.go
@@ -12,17 +12,36 @@ func main() {
 		fmt.Println("Error Occured")
 		return
 	}
-	defer file.Close()
-
-	for {
-		b := make([]byte, 8)
-		n, err := io.ReadFull(file, b)
-
-		if n > 0 {
-			fmt.Printf("read: %s\n", b[:n])
-		}
-		if err == io.EOF {
-			break
-		}
+	Lines := getLinesChannel(file)
+	for line := range Lines {
+		fmt.Printf("read: %s\n", line)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	var Channel chan string = make(chan string)
+	go func() {
+		defer f.Close()
+		oneLine := []byte{}
+		for {
+			b := make([]byte, 8)
+			n, err := f.Read(b)
+			for _, i := range b[:n] {
+				if i == '\n' {
+					Channel <- string(oneLine)
+					oneLine = []byte{}
+				} else {
+					oneLine = append(oneLine, i)
+				}
+			}
+			if err == io.EOF {
+				if len(oneLine) > 0 {
+					Channel <- string(oneLine)
+				}
+				break
+			}
+		}
+		close(Channel)
+	}()
+	return Channel
 }
