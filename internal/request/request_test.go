36e78440504e5bc66bd04ce78cb1230b7f2baaa6
CH <cedwards2809@gmail.com>
2026-05-11 09:40:20 -0400

    Ran Test

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,3 +1,13 @@
 package request
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
 // Unit Tests
+
+func TestRequestLineParse(t *testing.T) {
+	assert.Equal(t, "TheTestagen", "TheTestagen")
+}
