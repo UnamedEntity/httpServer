9a17b915e3daa55a5e9d2a1f3300b56fbffde4ed
CH <cedwards2809@gmail.com>
2026-05-14 20:19:44 -0400

    Tested for invalid charcters wehn parsing

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -13,6 +13,7 @@ func NewHeaders() Headers {
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	headers := strings.SplitN(string(data), "\r\n", 2)
+	validCharacters := "!#$%&'*+-.^_`|~ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 	if len(headers) == 1 {
 		return 0, false, nil
 	} else if headers[0] == "" {
@@ -21,10 +22,13 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	pair := strings.SplitN(headers[0], ":", 2)
 	if len(pair) != 2 {
 		return 0, false, errors.New("Missing \":\" ")
-	} else if pair[0] != strings.TrimSpace(pair[0]) {
-		return 0, false, errors.New("Too many spaces in header")
-	} else {
-		h[pair[0]] = strings.TrimSpace(pair[1])
-		return len(headers[0]) + 2, false, nil
 	}
+	for _, i := range pair[0] {
+		if strings.Contains(validCharacters, string(i)) == false {
+			return 0, false, errors.New("Invalid Characters")
+		}
+	}
+
+	h[strings.ToLower(pair[0])] = strings.TrimSpace(pair[1])
+	return len(headers[0]) + 2, false, nil
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -15,13 +15,57 @@ func TestHeadersParse(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
+
+	// Test: Valid single header with extra white space
+	headers = NewHeaders()
+	data = []byte("Host:         localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 31, n)
+	assert.False(t, done)
+
+	// Test: Valid 2 headers with exsiting headers
+	headers = NewHeaders()
+	data = []byte("Host:         localhost:42069\r\nLand: blalbla\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	n, done, err = headers.Parse(data[n:])
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, "blalbla", headers["land"])
+	assert.Equal(t, 15, n)
+	assert.False(t, done)
+
+	// Test: Valid Done
+	headers = NewHeaders()
+	data = []byte("Host:         localhost:42069\r\nLand: blalbla\r\n\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	total := n
+	n, done, err = headers.Parse(data[n:])
+	total += n
+	n, done, err = headers.Parse(data[total:])
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 0, n)
+	assert.True(t, done)
 
 	// Test: Invalid spacing header
 	headers = NewHeaders()
 	data = []byte("       Host: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Invalid chracters
+	headers = NewHeaders()
+	data = []byte("Host<>><><><><><><><: localhost<><><:42069\r\n\r\n")
 	n, done, err = headers.Parse(data)
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
